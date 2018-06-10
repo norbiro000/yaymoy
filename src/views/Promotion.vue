2139f2a7dcffe606bff77e421c1d62df2c7b7695
norbiro000 <boomtingtong@hotmail.com>
2018-06-11 00:01:07 +0700

    [Added] Promotions Mockup

--- src/views/Promotion.vue
+++ src/views/Promotion.vue
@@ -8,5 +8,19 @@
         ของคุณ มีอะไรบ้าง
       </p>
     </div>
+    <hr>
+    <PromotionsCard v-for="i in 1" :key="i"
+      :title="'ขนมจีน หลัง ม.อ.'"
+      :img="'https://www.cpbrandsite.com/contents/recipe/h84kygu5qngpv0xdl3tqcomtbuqllzy0tsqqzuzv.jpg'">
+    </PromotionsCard>
   </section>
 </template>
+
+<script>
+import PromotionsCard from '@/components/PromotionCard'
+export default {
+  components: {
+    PromotionsCard
+  }
+}
+</script>
